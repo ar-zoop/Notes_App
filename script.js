8a81283eb754465aa54175bca26540d4aff9f910
ar-zoop <arzoobapna01@gmail.com>
2022-11-18 22:54:13 +0530

    add notes on clicking submit button

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
 var dl = document.getElementById('note1');
-function createNote(){
+function createNote() {
 	//This creates a div
-	// const btn = document.createElement()
+	const newDiv = document.createElement("div");
+	newDiv.setAttribute("class", "col-sm-3 col-md-4 col-lg-4 ");
+	const newSec=document.createElement("section");
+	newSec.setAttribute("type", "button" );
+	newSec.setAttribute("data-bs-toggle", "modal");
+	newSec.setAttribute("data-bs-target", "#exampleModal");
+	newSec.setAttribute("class", "btn btn-primary card text-bg-light box chooseNote");
+	newSec.setAttribute("id", "note1");
+	newSec.setAttribute("data-bs-whatever", "Note 1");
+	newSec.setAttribute("style", "margin-right:9%");
+	//append the new note 
+	newDiv.appendChild(newSec);
+	const parent=document.getElementById("notesResidence");
+	parent.appendChild(newDiv);
 
 	//This updates the content
-	let x = document.getElementById('writeNote').value;
-	dl.innerHTML = x;
-	document.getElementById('modalText').innerHTML=x;
+		let x = document.getElementById('writeNote').value;
+		dl.innerHTML = x;
+		document.getElementById('modalText').innerHTML = x;
+		
+/*
+//This updates the content
+	try {
+		let x = document.getElementById('writeNote').value;
+
+		if (x == " ") {
+			throw "in throw block";
+		}
+		else{
+			dl.innerHTML = x;
+			document.getElementById('modalText').innerHTML = x;
+		}
+	}
+	catch(err){
+		window.alert("in catch block"+err);
+	}
+	finally {
+		window.alert("in finally block");
+	}
+*/
+
 }
 
-function updateNote(){
+function updateNote() {
 	let x = document.getElementById('modalText').value;
 	console.log(x);
 	dl.innerHTML = x;
 }
+
