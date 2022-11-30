38acf43a5b8f58a49a9e86dfb0244e14d23a92ce
ar-zoop <arzoobapna01@gmail.com>
2022-11-30 19:16:53 +0530

    Added alerts and local storage fetching

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-// var dl = document.getElementById('note1');
-var idCounter = 0;
-function createNote() {
-	//This creates a div 	
-	if (document.getElementById('writeNote').value == "" || document.getElementById('writeNote').value == " "){
-		return;
+function getAllNotes() {
+	let notes = JSON.parse(localStorage.getItem("notesapp-notes"));
+	if (notes==null) notes=[];
+	return notes;
+}
+ 
+function saveNotes(noteToSave) {
+	const notes = getAllNotes();
+	const existing= notes.find(note=> note.id == noteToSave.id);
+	if(existing){
+		existing.body=noteToSave.body;
 	}
+	else{
+		notes.push(noteToSave);
+	}
+	localStorage.setItem("notesapp-notes", JSON.stringify(notes));
+}
+
+function deleteANote(id) {
+	const notes= getAllNotes();
+	const newNotes=notes.filter(note=> note.id!=id);
+}
+
+
+// console.log(getAllNotes());
+
+function display(note){
+	//This creates a div 	
 	const newDiv = document.createElement("div");
 	newDiv.setAttribute("class", "col-sm-3 col-md-4 col-lg-4 ");
+	newDiv.setAttribute("id", "div" + note.id);
 
 	//This creates a sec
 	const newSec = document.createElement("section");
-	newSec.setAttribute("data-id", ++idCounter);
-	newSec.setAttribute("id", idCounter);
+	newSec.setAttribute("data-id", note.id);
+	newSec.setAttribute("id", note.id);
 	newSec.setAttribute("type", "button");
 	newSec.setAttribute("data-bs-toggle", "modal");
 	newSec.setAttribute("data-bs-target", "#exampleModal");
 	newSec.setAttribute("class", "btn btn-primary card text-bg-light box chooseNote");
-	// newSec.setAttribute("onclick", "display(this.id)");
 	newSec.setAttribute("style", "margin-right:9%");
 
 	//append the new note 
@@ -25,35 +46,62 @@ function createNote() {
 	parent.appendChild(newDiv);
 
 	//This updates the content
-	var x = document.getElementById('writeNote').value;
-	newSec.innerHTML = x;
-	document.getElementById('modalText').innerHTML = x;
+	var textToUpdate = note.body;
+	newSec.innerHTML = textToUpdate;
+	document.getElementById('modalText').innerHTML = textToUpdate;
 
-	/*
+}
+
+function loadNotes(){
+	const notes= getAllNotes();
+	console.log(notes);
+	if(notes!=[]){
+		notes.forEach(display);
+	}
+}
+
+function createNote() {
+	if (document.getElementById('writeNote').value == "" || document.getElementById('writeNote').value == " "){
+		return;
+	}
+	//Generate a random idCounter
+	let idCounter=Math.floor(Math.random()*1000000);
+
+	//This creates a div 	
+	const newDiv = document.createElement("div");
+	newDiv.setAttribute("class", "col-sm-3 col-md-4 col-lg-4 ");
+	newDiv.setAttribute("id", "div"+idCounter);
+
+	//This creates a sec
+	const newSec = document.createElement("section");
+	newSec.setAttribute("data-id", idCounter);
+	newSec.setAttribute("id", idCounter);
+	newSec.setAttribute("type", "button");
+	newSec.setAttribute("data-bs-toggle", "modal");
+	newSec.setAttribute("data-bs-target", "#exampleModal");
+	newSec.setAttribute("class", "btn btn-primary card text-bg-light box chooseNote");
+	newSec.setAttribute("style", "margin-right:9%");
+
+	//append the new note 
+	newDiv.appendChild(newSec);
+	const parent = document.getElementById("notesResidence");
+	parent.appendChild(newDiv);
+
 	//This updates the content
-		try {
-			let x = document.getElementById('writeNote').value;
-	
-			if (x == " ") {
-				throw "in throw block";
-			}
-			else{
-				dl.innerHTML = x;
-				document.getElementById('modalText').innerHTML = x;
-			}
-		}
-		catch(err){
-			window.alert("in catch block"+err);
-		}
-		finally {
-			window.alert("in finally block");
-		}
-	*/
+	var textToUpdate = document.getElementById('writeNote').value;
+	newSec.innerHTML = textToUpdate;
+	document.getElementById('modalText').innerHTML = textToUpdate;
+
+	//Save the note in local storage
+	saveNotes({
+		id:idCounter,
+		body:textToUpdate
+	});
 
 }
 
 
-
+//For updating data on "Save changes" button click in the modal
 $('#exampleModal').on('show.bs.modal', function (event) {
 	var button = $(event.relatedTarget);
 	var dataId = button.data('id'); 
@@ -65,9 +113,17 @@ $('#exampleModal').on('show.bs.modal', function (event) {
 		var textToUpdate = document.getElementById('modalText').value;
 		if(textToUpdate=="" || textToUpdate==" "){
 			document.getElementById(dataId).remove();
+			document.getElementById("div" + dataId).remove();
+			deleteANote(dataId);
 		}
 		else{
 			document.getElementById(dataId).innerHTML = textToUpdate;
+
+			//update in local storage
+			saveNotes({
+				id: dataId,
+				body: textToUpdate
+			});
 		}
 		dataId=null;
 	
@@ -76,11 +132,22 @@ $('#exampleModal').on('show.bs.modal', function (event) {
 	deleteNote.addEventListener('click', function handleClick() {
 
 		document.getElementById(dataId).remove();
-
+		document.getElementById("div"+dataId).remove();
+		deleteANote(dataId);
 	});
 });
  
 
+//alerts for updating
+$(document).ready(function () {
+	$('#updateNote').click(function () {
+		$('.alert-success').show()
+	})
+});
 
-
-
+//alert for deletion
+$(document).ready(function () {
+	$('#deleteNote').click(function () {
+		$('.alert-danger').show()
+	})
+});
